cfd5c2200918bbb4a15b5938a398093f3a28d67d
Mahalakshmi Murari <murarimaha@outlook.com>
2022-03-28 22:43:36 -0700

    Set1Task1 : Write your first C# code

---
+++ Maha.C.Sharp/Set1Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Set1Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Hi Everyone");
+                Console.Write("This is my first C# code");
+                Console.WriteLine("\nHi, I am C#.");
+                Console.Write("My age is 23.");
+                Console.Write("I score 99.8% in the programming");
+        }
+    }
+}
+   

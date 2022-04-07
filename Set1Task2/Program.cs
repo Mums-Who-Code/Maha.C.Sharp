20a6c5246aa6a65d549afb324680f17daf2e3881
Mahalakshmi Murari <murarimaha@outlook.com>
2022-04-06 21:31:46 -0700

    Set1Task2: Store and retrieve data using literal and variable values

---
+++ Set1Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Set1Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Hi, I am C#.";
+            string myAge = "My age is 23.";
+            string myMessage = "I score 99.8% in the programming.";
+            Console.WriteLine(myName);
+            Console.WriteLine(myAge);
+            Console.WriteLine(myMessage);
+        }
+    }
+}
